548a417992d52f2ca71c568e53c5b3625228268b
ggNomil <alimon927@gmail.com>
2025-03-30 13:07:39 -0500

    Adding project

---
+++ main.py
@@ -0,0 +1,31 @@
+﻿import sys
+
+from stats import count_words
+from stats import count_characters
+from sys import argv
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    if len(argv) < 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    counter = count_words(book_text)
+    characters = count_characters(book_text)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {counter} total words")
+    print("--------- Character Count --------")
+    for key in characters.keys():
+        print(f"{key}: {characters[key]}")
+    print("============= END ===============")
+
+main()
---
+++ stats.py
@@ -0,0 +1,22 @@
+﻿def count_words(text):
+    counter = 0
+    split_text = text.split()
+    for word in split_text:
+        counter += 1
+    return counter
+
+def sort_dictionary(dictionary):
+    sorted_dictionary = sorted(dictionary.items(), key=lambda item: item[1])
+def count_characters(text):
+    appearances = {}
+    lowercase_text = text.lower()
+    lowercase_text = lowercase_text.split()
+    for word in lowercase_text:
+        for char in word:
+            if char in appearances:
+                appearances[char] += 1
+            elif char not in appearances:
+                appearances[char] = 1
+
+    appearances_sorted = dict(sorted(appearances.items(), key=lambda item: item[1], reverse=True))
+    return appearances_sorted
